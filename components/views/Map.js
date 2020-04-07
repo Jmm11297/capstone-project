916021b4e5c249533ebc81ccae334b9f20fa0ca0
Jennifer McCormack <jmm11297@gmail.com>
2020-04-07 18:36:45 -0500

    Updates to API

--- components/views/Map.js
+++ components/views/Map.js
@@ -1,4 +1,24 @@
 export default () => `
 <div id="map">
 </div>
-`;
+`
+<script>
+    let map;
+    document.addEventListener("DOMContentLoaded", () => {
+        let s = document.createElement("script");
+        document.head.appendChild(s);
+        s.addEventListener("load", () => {
+            // script has loaded
+            console.log("script has loaded");
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: {
+                    lat:38.6270,
+                    lng:-90.1994
+                },
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+        });
+        s.src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDq4b9XG8Yfue3vTu-UC7FWO0BLppYGPMs";
+    });
+</script>;
