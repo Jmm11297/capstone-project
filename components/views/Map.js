a7a9161d63c1583e72ad22187b1c703a17b38973
Jennifer McCormack <jmm11297@gmail.com>
2020-04-07 19:36:16 -0500

    Remove async

--- components/views/Map.js
+++ components/views/Map.js
@@ -1,7 +1,7 @@
 export default () => `
 <div id="map">
 </div>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDq4b9XG8Yfue3vTu-UC7FWO0BLppYGPMs"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDq4b9XG8Yfue3vTu-UC7FWO0BLppYGPMs"></script>
 <script>
     let map = new google.maps.Map(document.getElementById("map"), {
         center: {lat:38.6270,lng:-90.1994},
